c58c0066fcba93373987a4250766a11a2b6469b5
Mihai Versan <versan.mihai@gmail.com>
2021-12-20 21:23:29 +0200

    default dependencies
    First commit

---
+++ RxJavaSampleApp/app/src/main/java/com/mihaiversan/rxjavasample/MainActivity.kt
@@ -0,0 +1,11 @@
+layout_post_list_itempackage com.mihaiversan.rxjavasample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
